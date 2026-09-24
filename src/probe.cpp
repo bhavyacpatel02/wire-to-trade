cba9f7272680c8fef49c79adea33137734b06851
bhavyapatel2002 <bhavyapatel2002@gmail.com>
2026-09-23 23:07:42 -0400

    bunch of other measurements

--- src/probe.cpp
+++ src/probe.cpp
@@ -1,5 +1,7 @@
+#include <chrono>
 #include <cstdint>
 #include <iostream>
+#include <thread>
 
 // This is the virtual timer counter, we're gonna use it as our timestamp
 uint64_t read_cntvct() {
@@ -23,8 +25,53 @@ uint64_t read_cntfrq() {
 
 int main() {
     auto freq = read_cntfrq();
-    auto cnt1 = read_cntvct();
-    auto cnt2 = read_cntvct();
 
-    std::cout << "Freq: " << freq << "\nCount1: " << cnt1 << "\nCount2: " << cnt2 << "\n";
+    auto cnt1 = read_cntvct();
+    auto tim1 = std::chrono::steady_clock::now();
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    auto cnt2 = read_cntvct();
+    auto tim2 = std::chrono::steady_clock::now();
+
+    std::chrono::duration<double> elapsed = tim2 - tim1;
+    std::cout << "Freq: " << freq << "\nCount1: " << cnt1
+              << "\nCount2: " << cnt2 << "\n";
+    auto adjustedFreq = (cnt2 - cnt1) / elapsed.count();
+    std::cout << "Adjusted Freq: " << adjustedFreq << "\n";
+
+    // Batch cost
+    auto start = std::chrono::steady_clock::now();
+    for (int i = 0; i < 1000000; ++i) {
+        read_cntvct();
+    }
+    auto end = std::chrono::steady_clock::now();
+
+    elapsed = end - start;
+    std::cout << "Batch cost per read: " << elapsed.count() / 1000000 << "\n";
+
+    // Check difference across many pairs of back-to-back reads
+    struct DiffCnts {
+        uint32_t zero{};
+        uint32_t one{};
+        uint32_t two{};
+    };
+
+    auto diffCnts = DiffCnts{};
+    auto prevCnt = read_cntvct();
+    for (int i = 1; i < 1000000; ++i) {
+        auto cnt = read_cntvct();
+        auto diff = cnt - prevCnt;
+        if (diff == 0) {
+            ++diffCnts.zero;
+        } else if (diff == 1) {
+            ++diffCnts.one;
+        } else if (diff == 2) {
+            ++diffCnts.two;
+        }
+
+        prevCnt = cnt;
+    }
+
+    std::cout << "Diff of 0: " << diffCnts.zero
+              << "\nDiff of 1: " << diffCnts.one
+              << "\nDiff of 2: " << diffCnts.two << "\n";
 }
